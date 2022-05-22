ef9b318927dee8e962e923577ae8adb0b6d2f055
samyquito <samyquito02@gmail.com>
2022-05-22 17:44:09 -0500

    Añadido funcionalidad SMS por Twilio y AWS, utilizando dotenv

--- server.py
+++ server.py
@@ -2,16 +2,20 @@ from flask import Flask, request
 from twilio.rest import Client
 import os
 import boto3
+from dotenv import load_dotenv
+load_dotenv()
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World Flask"
+    return "Hello, World Flask for notifications!"
 
 @app.route("/sms", methods=['POST'])
 def sms():
     hash = request.form['hash_validator']
-    if(hash == "Admin12345@2022Ucaldas"):
+    hash_validator=os.environ.get("hash_validator")
+    if(hash == hash_validator):
         destination = request.form['destination']
         message = request.form['message']
         client = "aws"
@@ -19,8 +23,8 @@ def sms():
             # Create an SNS client
             client = boto3.client(
                 "sns",
-                aws_access_key_id=os.environ["aws_access_key_id"],
-                aws_secret_access_key=os.environ["aws_secret_access_key"],
+                aws_access_key_id=os.environ.get("aws_access_key_id"),
+                aws_secret_access_key=os.environ.get("aws_secret_access_key"),
                 region_name="us-east-1"
             )
 
@@ -31,20 +35,21 @@ def sms():
             )
         else:
             try:
-                account_sid = os.environ["account_sid"]
-                auth_token = os.environ["auth_token"]
+                account_sid = os.environ.get("account_sid")
+                auth_token = os.environ.get("auth_token")
                 client = Client(account_sid, auth_token)
                 message = client.messages.create(
-                    messaging_service_sid=os.environ["messaging_service_sid"],
+                    messaging_service_sid=os.environ.get("messaging_service_sid"),
                     body=message,
                     to=destination
                 )
                 print(message.sid)
-                return "OK"
+                return "OK", 200
             except:
-                return "KO"
+                return "KO", 500
     else:
-        return "hash_error"
+        return "hash_error", 401
+
 
 if __name__ == '__main__':
     app.run()
