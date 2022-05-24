3adcc795ab3c090bdd5ed0ccbc064f077a9c7b44
estebanB616 <juan.1701617972@ucaldas.edu.co>
2022-05-24 13:22:23 -0500

    correos

--- server.py
+++ server.py
@@ -5,11 +5,38 @@ import boto3
 from dotenv import load_dotenv
 load_dotenv()
 
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, World Flask for notifications!"
+
+@app.route("/email", methods=['POST'])
+def email():
+    hash = request.form['hash_validator']
+    hash_validator=os.environ.get("hash_validator")
+    if(hash == hash_validator):
+        to = request.form['destination']
+        subject = request.form['subject']
+        message = request.form['message']
+        SENDGRID_API_KEY = os.environ["SENDGRID_API_KEY"]
+        message = Mail(
+            from_email=os.environ["SENDGRID_EMAIL_FROM"],
+            to_emails=to,
+            subject=subject,
+            html_content=message)
+        try:
+            sg = SendGridAPIClient(os.environ.get(SENDGRID_API_KEY))
+            response = sg.send(message)
+        except Exception as e:
+            print(e.message)
+    else:
+        return "hash_error"  
+
+
 
 @app.route("/sms", methods=['POST'])
 def sms():
