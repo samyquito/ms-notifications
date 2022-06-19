3bb2a9b7f3b92f5a6bb91e7fb975dfa55c1bd32b
samyquito <samyquito02@gmail.com>
2022-06-19 15:07:45 -0500

    Arreglado problema con Twilio, se eliminó el funcionamiento de AWS

--- server.py
+++ server.py
@@ -48,39 +48,20 @@ def sms():
     if(hash == hash_validator):
         destination = request.form['destination']
         message = request.form['message']
-        client = "aws"
-        if(client == "aws"):
-            try:
-            # Create an SNS client
-                client = boto3.client(
-                    "sns",
-                    aws_access_key_id=os.environ.get("aws_access_key_id"),
-                    aws_secret_access_key=os.environ.get("aws_secret_access_key"),
-                    region_name="us-east-1"
-                )
-
-                # Send your sms message.
-                client.publish(
-                    PhoneNumber=destination,
-                    Message=message
-                )
-                return "OK", 200
-            except:
-                return "KO", 500
-        else:
-            try:
-                account_sid = os.environ.get("account_sid")
-                auth_token = os.environ.get("auth_token")
-                client = Client(account_sid, auth_token)
-                message = client.messages.create(
-                    messaging_service_sid=os.environ.get("messaging_service_sid"),
-                    body=message,
-                    to=destination
-                )
-                print(message.sid)
-                return "OK", 200
-            except:
-                return "KO", 500
+        try:
+        # Create an SNS client
+            account_sid = os.environ.get("account_sid")
+            auth_token = os.environ.get("auth_token")
+            client = Client(account_sid, auth_token)
+            message = client.messages.create(
+                messaging_service_sid=os.environ.get("messaging_service_sid"),
+                body=message,
+                to=destination
+            )
+            print(message.sid)
+            return "OK", 200
+        except:
+             return "KO", 500
     else:
         return "hash_error", 401
 
